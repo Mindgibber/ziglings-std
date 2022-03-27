fbe7c587508ea657dd80938942c0b92262b07fa2
s0LA1337 <dream@neoncity.dev>
2022-03-27 01:46:59 +0000

    Create basic mockup for the first allocator lessons

---
+++ exercises/001_general_purpose_allocator.zig
@@ -0,0 +1,41 @@
+// 
+// 
+
+// pub const Config = struct {
+//     stack_trace_frames: usize = default_stack_trace_frames,
+//     enable_memory_limit: bool = false,
+//     safety: bool = std.debug.runtime_safety,
+//     thread_safe: bool = !builtin.single_threaded,
+//     MutexType: ?type = null,
+//     never_unmap: bool = false,
+//     retain_metadata: bool = false,
+//     verbose_log: bool = false,
+// };
+
+const std = @import("std");
+const heap = std.heap;
+
+const SomeImportantHeapObject = struct {
+    a: i128,
+    b: u128,
+};
+
+pub fn main() !void {
+    // First create an object of the General Purpose Allocator
+    const gpa = heap.GeneralPurposeAllocator(.{}){};
+
+    // Get the allocator instance
+    var allocator = gpa.allocator();
+
+    // Allocate our important heap object
+    var heap_object = allocator.create(SomeImportantHeapObject);
+
+    // Register the free call for the heap object
+    defer allocator.destroy();
+
+    // Derefence the heap object and initialize the struct
+    heap_object.* = .{
+        .a = 1,
+        .b = 2,
+    };
+}
